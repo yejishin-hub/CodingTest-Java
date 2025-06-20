1d2b7067d187d3f5af1646b58ae1bbcf09ed30e9
Yeji Shin <67362015+yejishin-hub@users.noreply.github.com>
2025-06-20 22:10:42 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < my_string.length(); i++) {
+            sb.append(my_string.substring(i, i + 1).repeat(n));
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
