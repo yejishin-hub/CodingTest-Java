77b995fca30f07bc87b2e9e95ee4b2bc015d6867
Yeji Shin <67362015+yejishin-hub@users.noreply.github.com>
2025-06-29 20:24:29 +0900

    [level 0] Title: A 강조하기, Time: 12.01 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(char c : myString.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                answer += Character.toLowerCase(c);
+            } else {
+                answer += c;
+            }
+        }
+        return answer.replaceAll("a", "A");
+    }
+}
