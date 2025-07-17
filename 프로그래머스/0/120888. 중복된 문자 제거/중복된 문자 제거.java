35ed2bb78d06c3e818980a48d852cf92f9b4057c
Yeji Shin <67362015+yejishin-hub@users.noreply.github.com>
2025-07-17 21:49:09 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.99 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] arr = my_string.split("");
+        for(String str : arr){
+		    if(!answer.contains(str)){            
+                answer += str;
+            }
+	    }
+        return answer;
+    }
+}
