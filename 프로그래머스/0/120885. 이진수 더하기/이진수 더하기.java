d0a8d96e2e2b15522eceb11ca6b23615a5d11680
Yeji Shin <67362015+yejishin-hub@users.noreply.github.com>
2025-08-03 17:41:23 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int num1 = Integer.parseInt(bin1,2);
+        int num2 = Integer.parseInt(bin2,2);
+        return Integer.toBinaryString(num1 + num2);
+    }
+}
