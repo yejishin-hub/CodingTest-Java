42a48835c88c45c110824d0164ee6e0615dacea3
Yeji Shin <67362015+yejishin-hub@users.noreply.github.com>
2025-08-04 10:43:11 +0900

    [level 1] Title: 자릿수 더하기, Time: 8.73 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String[] arr = (n+"").split("");
+        for(int i = 0; i < arr.length; i++) {
+            answer += Integer.parseInt(arr[i]);
+        }
+        return answer;
+    }
+}
